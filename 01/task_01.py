b5f411549ae16f89701ae3780e98797a24979224
Zapadynsky <e.zapadinsky@gmail.com>
2022-11-25 20:05:06 +0200

    Add all data for check creating

--- 01/task_01.py
+++ 01/task_01.py
@@ -14,20 +14,26 @@ class ShoppingCart:
     """Describing the shopping cart"""
 
     def __init__(self):
-        self.total_cart = []
-        self.total_count = 0
+        self.products_list = {}
 
     def add(self, product: Product, count: float = 1):
-        """Adding products to the cart, count products"""
+        """Adding products to the cart"""
 
-        total_product_price = product.get_total(count)
-        self.total_cart.append(total_product_price)
-        self.total_count += count
+        if product.name in self.products_list:
+            quantity = self.products_list.get(product.name, 0)["quantity"] + count
+        else:
+            quantity = count
+        product_sum = product.get_total(quantity)
+        price = product.price
+        self.products_list[product.name] = {"quantity": quantity, "price": price, "product_sum": product_sum}
 
     def total_cart_sum(self):
         """Calculating the total cart sum"""
 
-        return sum(self.total_cart)
+        product_list_sum = 0
+        for product in self.products_list:
+            product_list_sum += self.products_list[product]['product_sum']
+        return product_list_sum
 
 
 prod1 = Product('lemon', 15.10)
@@ -39,5 +45,5 @@ cart1.add(prod1, 0.7)
 cart1.add(prod2, 20)
 cart1.add(prod3, 20)
 cart1.add(prod1, 5)
+print(cart1.products_list)
 print(cart1.total_cart_sum())
-print(cart1.total_count)
